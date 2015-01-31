2b1f508bd952721afc474aa6e7965c621b8da83e
Garrett Cox <garrettjcox@gmail.com>
2015-01-31 02:06:46 -0800

    Refactor : stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,55 +1,28 @@
-var stringifyJSON = function(obj) {
-  var stringify = {
-    "number": function(number){
-        return number.toString();
-    },
-    "object": function(object){
-        if (object === null){
-            return "null";
-        }else if(Array.isArray(object)){
-            var string = "[";
-            var first = true;
-            for (var i = 0; i < object.length; i++) {
-                if (first){
-                    first = false;
-                }else{
-                    string += ","
-                }
-                var itemType = typeof object[i]
-                string += stringifyJSON(object[i]);
-            };
-            string += "]"
-            return string;
-        }else{
-            var string = "{";
-            var first = true;
-            for (var key in object){
-                var itemType = typeof object[key];
-                if(itemType !== "undefined" && itemType !== "function"){
-                    if(first){
-                        first = false;
-                    }else{
-                        string += ","
-                    }
-                    string += '"'+key+'":'+stringifyJSON(object[key]);
-                }
-            }
-            string += "}"
-            return string;
-        }
-
-    },
-    "boolean": function(bool){
-        if(bool){
-            return "true";
-        }else{
-            return "false";
-        }
-    },
-    "string": function(str){
-        return '"'+str+'"';
+var stringIt = function(){}
+stringIt.prototype.number = String;
+stringIt.prototype.boolean = String;
+stringIt.prototype.null = String;
+stringIt.prototype.string = function(string){return '"' + string + '"'};
+stringIt.prototype.array = function(array){return '[' + _.map(array, stringifyJSON) + ']'};
+stringIt.prototype.object = function(object){
+    var string = '{';
+    var first = true;
+    for (var key in object){
+        if (typeof object[key] === "undefined" || typeof object[key] === "function") break;
+        if (first) first = false;
+        else string += ','
+        string += stringifyJSON(key) + ':' + stringifyJSON(object[key]);
     }
-  }
-  var type = typeof obj
-  return stringify[type](obj);
+    string += '}'
+    return string
+}
+
+var stringifyJSON = function(obj) {
+    var string = new stringIt();
+    var type = typeof obj;
+    if (type === 'object'){
+        if (obj === null){type = 'null'}
+        else if (Array.isArray(obj)){type = 'array'}
+    }
+    return string[type](obj);
 };
