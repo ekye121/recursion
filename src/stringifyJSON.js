70084f50e0c1640c9430e3e79ad2533218140a22
garrettjoecox <garrettjcox@gmail.com>
2015-02-07 15:36:01 -0800

    Modify formatting and spacing

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,17 +2,18 @@ var stringIt = function(){}
 stringIt.prototype.number = String;
 stringIt.prototype.boolean = String;
 stringIt.prototype.null = String;
-stringIt.prototype.string = function(string){return '"' + string + '"'};
-stringIt.prototype.array = function(array){return '[' + _.map(array, stringifyJSON) + ']'};
-stringIt.prototype.object = function(object){
+stringIt.prototype.string = function(string) { return '"' + string + '"' };
+stringIt.prototype.array = function(array) { return '[' + _.map(array, stringifyJSON) + ']' };
+stringIt.prototype.object = function(object) {
     var results = [];
-    _.each(object, function(item, index){
-        if (typeof item !== 'undefined' && typeof item !== 'function'){
+    _.each(object, function(item, index) {
+        if (typeof item !== 'undefined' && typeof item !== 'function') {
             results.push('"' + index + '"' + ':' + stringifyJSON(item))
-        }
-    })
-    return '{' + results.join(',') + '}'
+        };
+    });
+    return '{' + results.join(',') + '}';
 };
+
 var stringifyJSON = function(obj) {
     var string = new stringIt();
     var type = typeof obj;
