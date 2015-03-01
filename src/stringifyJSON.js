18aa44077d7aefdea6c5501b6ecb506c373ec14b
garrettjoecox <garrettjcox@gmail.com>
2015-03-01 15:16:33 -0800

    Fixed typo that broke function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,23 +1,23 @@
-var stringIt = function(){}
-stringIt.prototype.number = String;
-stringIt.prototype.boolean = String;
-stringIt.prototype.null = String;
-stringIt.prototype.string = function(string) { return '"' + string + '"' };
-stringIt.prototype.array = function(array) { return '[' + _.map(array, stringifyJSON) + ']' };
-stringIt.prototype.object = function(object) {
-    var results = [];
-    _.each(object, function(item, index) {
-        if (typeof item !== 'undefined' && typeof item !== 'function') {
-            results.push('"' + index + '"' + ':' + stringifyJSON(item))
-        };
-    });
-    return '{' + results.join(',') + '}';
+var stringIt = {
+    number: String,
+    boolean: String,
+    null: String,
+    string: function(string) { return '"' + string + '"'; },
+    array: function(array) { return '[' + _.map(array, stringifyJSON) + ']'; },
+    object: function(object) {
+        var results = [];
+        _.each(object, function(item, key) {
+            if (typeof item !== 'undefined' && typeof item !== 'function') {
+                results.push('"' + key + '"' + ':' + stringifyJSON(item));
+            }
+        });
+        return '{' + results.join(',') + '}';
+    }
 };
 
-var stringifyJSON = function(obj) {
-    var string = new stringIt();
+function stringifyJSON(obj) {
     var type = typeof obj;
-    if (obj === null) type = 'null';
+    if (!obj) type = 'null';
     if (Array.isArray(obj)) type = 'array';
-    return string[type](obj);
-};
+    return stringIt[type](obj);
+}
